1bed2b2c231cb337d1f3476574495a734f86d90f
hadera6 <haderateame631@gmail.com>
2026-06-14 22:41:55 +0200

    reshape labels to 1D long tensor in data.py

--- Code/data.py
+++ Code/data.py
@@ -16,13 +16,14 @@ def get_loaders(data, data_path, batch_size, val_split=0.1):
     val_start = total_samples - val_size
 
     train_data   = data_dict['train_images'][:val_start]
-    train_labels = data_dict['train_labels'][:val_start]
+    train_labels = data_dict['train_labels'][:val_start].squeeze().long()
     val_data = data_dict['train_images'][val_start:]
-    val_labels = data_dict['train_labels'][val_start:]
+    val_labels = data_dict['train_labels'][val_start:].squeeze().long()
+    test_labels  = data_dict['test_labels'].squeeze().long()
     
     train_dataset = TensorDataset(train_data, train_labels)
     val_dataset = TensorDataset(val_data, val_labels)
-    test_dataset = TensorDataset(data_dict['test_images'], data_dict['test_labels'])
+    test_dataset = TensorDataset(data_dict['test_images'], test_labels)
     
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
