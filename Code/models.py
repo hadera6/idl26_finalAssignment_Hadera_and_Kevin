dc5a31da222f97f70717651786dd802e4bbcaef5
hadera6 <haderateame631@gmail.com>
2026-06-14 21:47:35 +0200

    add per-convolution padding rule to VGGBlock in models.py

--- Code/models.py
+++ Code/models.py
@@ -21,6 +21,7 @@ class VGGBlock(nn.Module):
         for i in range(num_convs):
             is_config_c_tail = (num_convs == 3 and i == 2)
             kernel_size = 1 if is_config_c_tail else 3
+            padding     = 0 if is_config_c_tail else 1
             layers.append(nn.Conv2d(current_in_channels, out_channels, kernel_size=kernel_size, padding=padding))
             layers.append(nn.BatchNorm2d(out_channels))
             layers.append(nn.ReLU(inplace=True))
