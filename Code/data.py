32e83799c8e0773b4fa1c67ccecd29b8739ef2e0
hadera6 <haderateame631@gmail.com>
2026-06-21 23:35:31 +0200

    add normalisation to stabilise training in data.py

--- Code/data.py
+++ Code/data.py
@@ -19,11 +19,20 @@ def get_loaders(data, data_path, batch_size, val_split=0.1):
     train_labels = data_dict['train_labels'][:val_start].squeeze().long()
     val_data = data_dict['train_images'][val_start:]
     val_labels = data_dict['train_labels'][val_start:].squeeze().long()
+    test_data    = data_dict['test_images']
     test_labels  = data_dict['test_labels'].squeeze().long()
+
+    mean = train_data.mean(dim=[0, 2, 3], keepdim=True)   
+    std  = train_data.std(dim=[0, 2, 3],  keepdim=True)   
+    std  = std.clamp(min=1e-8)                             
+
+    train_data = (train_data - mean) / std                 
+    val_data   = (val_data   - mean) / std
+    test_data  = (test_data  - mean) / std
     
     train_dataset = TensorDataset(train_data, train_labels)
     val_dataset = TensorDataset(val_data, val_labels)
-    test_dataset = TensorDataset(data_dict['test_images'], test_labels)
+    test_dataset  = TensorDataset(test_data,   test_labels)
     
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
